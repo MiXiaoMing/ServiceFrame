097d82b01abf7826d9abc2c216087008fd4d3e11
mixiaoming <mixiaoming@qiumiapp.com>
2020-05-27 09:28:36 +0800

    修改文件名

--- docker-restart.sh
+++ docker-restart.sh
@@ -4,5 +4,5 @@ set timeout -1
 expect "root@1.119.166.179's password: "
 send "LqIkKKTIcKeTIXSF4FF6Ftp\r"
 expect "*]#"
-send "docker-compose -f /jbh/servers/docker-compose.yaml restart [lindex $argv 0] && exit\r"
+send "docker-compose -f /jbh/servers/docker-compose-serviceframe.yaml restart [lindex $argv 0] && exit\r"
 expect eof
